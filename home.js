25d309662f37031ea0f47b5c4cae8cdf3779c657
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-28 22:13:05 +0530

    tutorials

---
+++ home.js
@@ -0,0 +1,75 @@
+// function add(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+// add(2,6);
+
+// const Name = "holmes";l
+
+// function toUpper(text) {
+//     const upperCased = text.toUpperCase();
+//     console.log(upperCased);
+// }
+
+// toUpper(Name);
+
+
+// const myName = sru;
+// const age = 19;
+
+// console.log (`hello its me ${myName} n my ag is ${age}`);
+
+// const add =  myName + age;
+// console.log(add);
+
+
+// /home/srushti/tutorials /index2.js   path of this file
+
+
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('Text').innerHTML = age;
+
+// // function  :))
+// function sumnum(a, b) {
+//     var c = a+b ;
+//     console.log(c);
+// }
+
+// while loops :))
+
+// var num = 1; 
+// var a = 1;
+// while (num <100) {
+//     num *= a ;
+//     console.log(a, num);
+//     a++;
+// }
+
+// for loops :))
+
+// for (let a = 1; a <= 11 ; a++){
+//     {
+//         console.log(a);
+//     }
+// }
+
+// Data types :))
+
+// let yourage = 19;  // numbers
+// let urname = "sru"; // string
+// let name = { first : 'sru', last : "kul"}; //  object
+// let truth = false;   // boolen
+// let list = [ 'coffee', 'apple', 'books'];// arrys
+// let nothing = null; // value none
+
+
+//  Strings in JavaScript
+let fruit = 'apple'
+let morefruits= 'apple,banana,kiwi'
+console.log(fruit.length);
+console.log(fruit.indexOf('ppl'));
+console.log(fruit.slice(0,15));
+console.log(fruit.replace('ppl' , '222'));
+console.log(fruit.split('<3'))
