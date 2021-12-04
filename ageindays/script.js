07c9dbcd9ee8f59d4be33433e41df2a84fb399ea
srushti0415 <srushtikulkarni15@gmail.com>
2021-12-04 22:01:06 +0530

    complated

--- ageindays/script.js
+++ ageindays/script.js
@@ -1,1 +1,15 @@
-console.log('hi')
+// challange in days
+function Ageindays() {
+    var birthyear = prompt ('What year you where born ? ');
+    var Ageindays = (2021 - birthyear) *365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + Ageindays + ' days old ');
+    h1.setAttribute('id', 'Ageindays')
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    // console.log(Ageindays);
+};
+
+function reset() {
+    document.getElementById('Ageindays').remove();
+}
